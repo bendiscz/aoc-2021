6915bbeb40c2b70035142b8a203275ab3fd7f467
Martin Benda <martin@bendovi.cz>
2021-12-22 16:26:13 +0100

    day22: initial import

---
+++ day22a/day22a.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"fmt"
+	"github.com/bendiscz/aoc-2021"
+	"regexp"
+)
+
+//go:embed input1
+var input []byte
+
+var pattern = regexp.MustCompile(`^(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)$`)
+
+const (
+	S = 50
+	D = 2*S + 1
+)
+
+type space [D][D][D]bool
+
+func (s *space) count() (count int) {
+	for x := 0; x < D; x++ {
+		for y := 0; y < D; y++ {
+			for z := 0; z < D; z++ {
+				if s[x][y][z] {
+					count++
+				}
+			}
+		}
+	}
+	return
+}
+
+func main() {
+	var c space
+	scanner := bufio.NewScanner(bytes.NewReader(input))
+	for scanner.Scan() {
+		if len(scanner.Text()) == 0 {
+			break
+		}
+		m := pattern.FindStringSubmatch(scanner.Text())
+		x1, x2, cx := parseInterval(m[2], m[3])
+		y1, y2, cy := parseInterval(m[4], m[5])
+		z1, z2, cz := parseInterval(m[6], m[7])
+		if cx || cy || cz {
+			continue
+		}
+		b := m[1] == "on"
+
+		for x := x1; x <= x2; x++ {
+			for y := y1; y <= y2; y++ {
+				for z := z1; z <= z2; z++ {
+					c[x][y][z] = b
+				}
+			}
+		}
+	}
+
+	fmt.Printf("part one: %v\n", c.count())
+}
+
+func parseInterval(a, b string) (int, int, bool) {
+	x, cx := clamp(aoc.ParseInt(a))
+	y, cy := clamp(aoc.ParseInt(b))
+	return x + S, y + S, cx || cy
+}
+
+func clamp(x int) (int, bool) {
+	clamped := false
+	if x < -S {
+		x = -S
+		clamped = true
+	}
+	if x > S {
+		x = S
+		clamped = true
+	}
+	return x, clamped
+}
---
+++ day22b/day22b.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"fmt"
+	"github.com/bendiscz/aoc-2021"
+	"math"
+	"regexp"
+)
+
+//go:embed input1
+var input []byte
+
+var pattern = regexp.MustCompile(`^(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)$`)
+
+type box struct {
+	a     [3]axis
+	value bool
+}
+
+type axis struct {
+	p, q int
+}
+
+type interval struct {
+	p, q  int
+	boxes map[int]struct{}
+}
+
+func (i *interval) size() int {
+	return i.q - i.p + 1
+}
+
+func (i *interval) add(b int) {
+	i.boxes[b] = struct{}{}
+}
+
+func (i *interval) has(b int) bool {
+	_, ok := i.boxes[b]
+	return ok
+}
+
+func (i *interval) copy() *interval {
+	i2 := interval{
+		i.p, i.q, make(map[int]struct{}),
+	}
+	for b := range i.boxes {
+		i2.boxes[b] = struct{}{}
+	}
+	return &i2
+}
+
+func inf() *interval {
+	return &interval{
+		p:     math.MinInt,
+		q:     math.MaxInt,
+		boxes: map[int]struct{}{},
+	}
+}
+
+func main() {
+	var boxes []box
+	var sxs = []*interval{inf()}
+	var sys = []*interval{inf()}
+	var szs = []*interval{inf()}
+
+	scanner := bufio.NewScanner(bytes.NewReader(input))
+	for scanner.Scan() {
+		m := pattern.FindStringSubmatch(scanner.Text())
+		x1, x2 := aoc.ParseInt(m[2]), aoc.ParseInt(m[3])
+		y1, y2 := aoc.ParseInt(m[4]), aoc.ParseInt(m[5])
+		z1, z2 := aoc.ParseInt(m[6]), aoc.ParseInt(m[7])
+
+		i := len(boxes)
+		b := makeBox(x1, x2, y1, y2, z1, z2, m[1] == "on")
+		boxes = append(boxes, b)
+
+		sxs = insert(sxs, i, x1, x2)
+		sys = insert(sys, i, y1, y2)
+		szs = insert(szs, i, z1, z2)
+	}
+
+	volume := 0
+	for k, sx := range sxs {
+		fmt.Printf("%d/%d\n", k, len(sxs))
+		ax := map[int]struct{}{}
+		for bx := range sx.boxes {
+			ax[bx] = struct{}{}
+		}
+		if len(ax) == 0 {
+			continue
+		}
+
+		for _, sy := range sys {
+			ay := map[int]struct{}{}
+			for by := range sy.boxes {
+				if _, ok := ax[by]; ok {
+					ay[by] = struct{}{}
+				}
+			}
+			if len(ay) == 0 {
+				continue
+			}
+
+			for _, sz := range szs {
+				last := -1
+				for b := range ay {
+					if b > last && sz.has(b) {
+						last = b
+					}
+				}
+
+				if last >= 0 && boxes[last].value {
+					volume += sx.size() * sy.size() * sz.size()
+				}
+			}
+		}
+	}
+
+	fmt.Printf("part two: %v\n", volume)
+}
+
+func makeBox(x1, x2, y1, y2, z1, z2 int, value bool) box {
+	return box{
+		a: [...]axis{
+			{x1, x2},
+			{y1, y2},
+			{z1, z2},
+		},
+		value: value,
+	}
+}
+
+func insert(ss []*interval, b, p, q int) []*interval {
+	var next []*interval
+	for _, s := range ss {
+		if p > s.p && p <= s.q {
+			s2 := s.copy()
+			s2.q = p - 1
+			s.p = p
+			next = append(next, s2)
+		}
+
+		if q >= s.p && q < s.q {
+			s2 := s.copy()
+			s2.q = q
+			s.p = q + 1
+			next = append(next, s2)
+		}
+
+		next = append(next, s)
+	}
+
+	for _, s := range next {
+		if p <= s.p && q >= s.q {
+			s.add(b)
+		}
+	}
+
+	return next
+}
